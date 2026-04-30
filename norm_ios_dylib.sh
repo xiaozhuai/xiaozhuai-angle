a1f473558e1b7a3c5f257819d2314142f9a914c7
xiaozhuai <798047000@qq.com>
2026-04-30 18:11:57 +0800

    [iOS] Switch from dylib to framework

--- norm_ios_dylib.sh
+++ norm_ios_dylib.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-install_name_tool -id "@rpath/libEGL.dylib" lib/ios-arm64/libEGL.dylib
-install_name_tool -id "@rpath/libGLESv2.dylib" lib/ios-arm64/libGLESv2.dylib
-
-codesign -f -s - lib/ios-arm64/libEGL.dylib
-codesign -f -s - lib/ios-arm64/libGLESv2.dylib
-
-otool -L lib/ios-arm64/libEGL.dylib
-otool -L lib/ios-arm64/libGLESv2.dylib
+otool -L lib/ios-arm64/libEGL.framework/libEGL
+otool -L lib/ios-arm64/libGLESv2.framework/libGLESv2
